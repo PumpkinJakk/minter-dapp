e3e6d1c2714cfcfdfa34ca93e341936a3b17c430
PumpkinJakk <jakkvillagenft@gmail.com>
2022-07-16 13:26:50 +0800

    WL

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x4a201ce77882D7AF103329c319fF9B4a6FC64CaC"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0x4a201ce77882D7AF103329c319fF9B4a6FC64CaC","0xf271aBE09cBF2974667649457a6EC3767DC96822"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xf271aBE09cBF2974667649457a6EC3767DC96822"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
